a6264f580cdec34f843aeb285c190672e7bb7848
hiwatashi-michiya <115054875+hiwatashi-michiya@users.noreply.github.com>
2024-05-30 02:41:52 +0900

    黄金ゲットしている時はマグマ上昇速度が少し早くなるように設定

--- DirectXGame/Game/Stage/Magma.h
+++ DirectXGame/Game/Stage/Magma.h
@@ -34,7 +34,8 @@ public:
 	//マグマ制限
 	float magmaLimit_ = Block::kBlockSize_ * 6.0f;
 	//マグマ速度
-	float magmaSpeed_ = 1.5f;
+	float magmaSpeed_ = 1.0f;
+	float magmaSpeedFaster_ = 1.5f;
 	//硬直時間
 	int32_t freezeTime_ = 0;
 	//イージング変数
